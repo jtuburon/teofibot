01390db702f01e7bdbe17c94b2333c6b07ecc7fc
teo <jtuburon@gmail.com>
2016-12-13 14:44:12 -0500

    temporary service to turn on tv

--- teofibotweb/views.py
+++ teofibotweb/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from subprocess import call
+
+def send_once(device_id, message):
+	call(['irsend', 'SEND_ONCE', device_id, message])
 
 # Create your views here.
+def control(request):
+	command=request.GET.get('command', '')
+	print command
+	if command=="PWR_ON":
+		print "PRENDERE TV"
+		send_once("LG", "lg_power")
+		print "PRENDI TV"
+	return HttpResponse('OK')
+
