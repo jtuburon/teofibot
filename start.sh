480a21d53409de25dd91e5d081916c8b479c964b
Teo29 <jtuburon@gmail.com>
2017-08-24 22:59:21 -0500

    Database migrated to MYSQL

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 cd /home/pi/teofibot
-nohup python manage.py runserver 0.0.0.0:8000 &
 nohup python manage.py initbot &
 cd
--- teofibot/settings.py
+++ teofibot/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'teofibot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'teofibot_db',
+        'USER': 'teofibot',
+        'PASSWORD': 't30f1b0t',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
